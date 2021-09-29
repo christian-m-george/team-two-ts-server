121eee00c348e30fd2e138ca315c8b44b566b0c8
halfpasta <christian.george360@gmail.com>
2021-09-29 14:46:50 -0400

    added comments

--- src/auth/authRouter.ts
+++ src/auth/authRouter.ts
@@ -1,16 +1,28 @@
 import express, { Request, Response, NextFunction, Router } from "express";
+import userMethods from "../_prismaClient/_prismaClient";
 
 const authRouter: Router = express.Router();
 
-authRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body, "this is the req body");
-  res.json('hit auth get');
-  next(console.log('yes'));
+// authRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   console.log(req.body, "this is the req body");
+//   res.json();
+// });
+
+// Handles User logins
+// Needs to send an authorization token to client
+authRouter.post("/", async (req: Request, res: Response, next: NextFunction) => {
+  const userEmail: string = req.body.email;
+  const userPassword: string = req.body.password;
+  if (!userEmail) res.send('missing email');
+  if (!userPassword) res.send('missing password');
+  else {
+      const myUser = await userMethods.getUserByEmail(userEmail);
+      console.log("THIS IS USER: " + myUser?.password)
+      const checkPassword = myUser?.password;
+      if (checkPassword === userPassword) res.send('succesful login')
+      else res.send("invalid username or password");
+  }
 });
 
-authRouter.post("/", (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.body, "this is the req body");
-  res.json();
-});
 
 export default authRouter;
