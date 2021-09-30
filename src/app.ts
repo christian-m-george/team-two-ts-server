075e747034453652a8408c24144fd75ce7f92a24
halfpasta <christian.george360@gmail.com>
2021-09-29 20:57:50 -0400

    updated express app cors settings

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 import express, { Application } from 'express';
 import cors from "cors";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app: Application = express();
 
-app.use(cors());
+app.use(cookieParser())
+app.use(cors({
+    origin : "http://localhost:3000",
+    credentials: true,
+  }));
 app.use(express.json())
 
 export default app;
