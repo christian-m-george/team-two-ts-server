38fa43e1ac5820838501eb07208d1335d465a44d
halfpasta <christian.george360@gmail.com>
2021-09-29 20:56:14 -0400

    jwt signing file

---
+++ src/utils/signJWT.ts
@@ -0,0 +1,33 @@
+import config from '../config';
+import jwt from 'jsonwebtoken';
+
+const signJWT = (email: string, callback: (error: Error | null, token: string | null) => void): void => {
+    const timeSinceEpoch = new Date().getTime();
+    const expirationTime = timeSinceEpoch + Number(config.token.expireTime) * 100000;
+    const expirationTimeInSeconds = Math.floor(expirationTime / 1000);
+
+    try {
+        jwt.sign(
+        {
+            email: email,
+        },
+            config.token.tokenSecret,
+        {
+            issuer: config.token.issuer,
+            algorithm: "HS256",
+            expiresIn: expirationTimeInSeconds
+        },
+        (error, token) => {
+            if(error) {
+                callback(error, null);
+            } else if (token) {
+                callback(null, token);
+            }
+        }
+        )
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+export default signJWT;
