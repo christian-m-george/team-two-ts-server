38cc7f0569c4ff98d8c2d8fca94360e4c77d7e37
halfpasta <christian.george360@gmail.com>
2021-09-29 20:57:27 -0400

    updated config for jwt

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const NODE_ENV: string = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8000;
+const SERVER_TOKEN_EXPIRE_TIME = process.env.SERVER_TOKEN_EXPIRE_TIME || 3600;
+const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'questioneer';
+const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'backupsecretitisntsogood';
 
 const config = {
     NODE_ENV: NODE_ENV,
-    PORT: PORT
-}
+    PORT: PORT,
+    token: {
+        expireTime: SERVER_TOKEN_EXPIRE_TIME,
+        issuer: SERVER_TOKEN_ISSUER,
+        tokenSecret: SERVER_TOKEN_SECRET
+    }
+};
 
 export default config;
