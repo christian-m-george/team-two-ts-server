826a1a1b3180a6cdf2a44bba833ed54314acaa40
halfpasta <christian.george360@gmail.com>
2021-10-06 21:03:51 -0400

    config change

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ const PORT = process.env.PORT || 8000;
 const SERVER_TOKEN_EXPIRE_TIME = process.env.SERVER_TOKEN_EXPIRE_TIME || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'questioneer';
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'backupsecretitisntsogood';
+const REFRESH_TOKEN_EXPIRE_TIME = process.env.REFRESH_TOKEN_EXPIRE_TIME || 3600000;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'refreshtokensecretlol!!!';
 
 const config = {
     NODE_ENV: NODE_ENV,
