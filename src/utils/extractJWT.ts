33a2eb095e84984280f025b29c8da9e5f6a5224d
halfpasta <christian.george360@gmail.com>
2021-10-08 12:50:50 -0400

    can now pass function as middleware to routes

--- src/utils/extractJWT.ts
+++ src/utils/extractJWT.ts
@@ -1,25 +1,32 @@
 import config from '../config';
 import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
+import Cookie from './cookie';
 
 const extractJWT = (req: Request, res: Response, next: NextFunction) => {
-    let token = req.headers.authorization?.split(' ')[1];
-
-    if(token) {
-        jwt.verify(token, config.token.tokenSecret, (error, decoded) => {
+    const cookie: Cookie = req.cookies;
+    console.log(cookie);
+    console.log(JSON.stringify(cookie));
+    const hash = cookie.acctok;
+    // const payload = hash.split('.')[1]
+    
+    if(hash) {
+        jwt.verify(hash, config.token.tokenSecret, async (error, decoded) => {
             if (error) {
+                console.log(error);
                 return res.status(400).json({
                     message: error.message,
                     error
                 });
             }
             else {
+                console.log('here 3');
                 res.locals.jwt = decoded;
                 next();
             }
         })
     } else {
-        res.status(401).json('unauthorized');
+        res.status(401).json('not this time bozo');
     }
 }
 
