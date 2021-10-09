bb4df264505067ed71c8bfd3718d39be32bef385
halfpasta <christian.george360@gmail.com>
2021-10-09 01:40:29 -0400

    config updated

--- build/config.js
+++ build/config.js
@@ -5,13 +5,17 @@ const PORT = process.env.PORT || 8000;
 const SERVER_TOKEN_EXPIRE_TIME = process.env.SERVER_TOKEN_EXPIRE_TIME || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || 'questioneer';
 const SERVER_TOKEN_SECRET = process.env.SERVER_TOKEN_SECRET || 'backupsecretitisntsogood';
+const REFRESH_TOKEN_EXPIRE_TIME = process.env.REFRESH_TOKEN_EXPIRE_TIME || 3600000;
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'refreshtokensecretlol!!!';
 const config = {
     NODE_ENV: NODE_ENV,
     PORT: PORT,
     token: {
         expireTime: SERVER_TOKEN_EXPIRE_TIME,
         issuer: SERVER_TOKEN_ISSUER,
-        tokenSecret: SERVER_TOKEN_SECRET
+        tokenSecret: SERVER_TOKEN_SECRET,
+        refreshSecret: REFRESH_TOKEN_SECRET,
+        refreshExpireTime: REFRESH_TOKEN_EXPIRE_TIME
     }
 };
 exports.default = config;
