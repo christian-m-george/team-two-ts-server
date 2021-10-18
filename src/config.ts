471be22ec4af822a322a4180c5f92a2ed658d4b9
halfpasta <christian.george360@gmail.com>
2021-10-17 23:18:53 -0400

    updated

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,11 @@ const REFRESH_TOKEN_SECRET: string | Buffer | {
     key: string | Buffer;
     passphrase: string;
 } = process.env.REFRESH_TOKEN_SECRET || 'refreshtokensecretlol!!!';
+const SURVEY_TOKEN_EXPIRE_TIME = process.env.REFRESH_TOKEN_EXPIRE_TIME || 3600000;
+const SURVEY_TOKEN_SECRET: string | Buffer | {
+    key: string | Buffer;
+    passphrase: string;
+} = process.env.REFRESH_TOKEN_SECRET || 'dufahds@&*#EHQBFUESbfuaesb!!!';
 
 const config = {
     NODE_ENV: NODE_ENV,
@@ -20,7 +25,9 @@ const config = {
         issuer: SERVER_TOKEN_ISSUER,
         tokenSecret: SERVER_TOKEN_SECRET,
         refreshSecret: REFRESH_TOKEN_SECRET,
-        refreshExpireTime: REFRESH_TOKEN_EXPIRE_TIME
+        refreshExpireTime: REFRESH_TOKEN_EXPIRE_TIME,
+        surveySecret: SURVEY_TOKEN_SECRET,
+        surveyExpireTime: SURVEY_TOKEN_EXPIRE_TIME
     },
     VERCEL_URL: `https://team-two-client.vercel.app`,
     REACT_URL: `http://localhost:3000`
