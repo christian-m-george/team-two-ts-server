9bb2c04d3e790dbd01891f58f6d8776871362e39
halfpasta <christian.george360@gmail.com>
2021-10-21 15:02:26 -0400

    added get responses by survey id path

--- src/response/responseRouter.ts
+++ src/response/responseRouter.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
+import extractJWT from '../utils/extractJWT';
 import dbMethods from '../_prismaClient/_prismaClient';
 // import dbMethods from '../_prismaClient/_prismaClient';
 // import jwt from 'jsonwebtoken';
@@ -19,14 +20,30 @@ responseRouter.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.json('get accessed');
 })
 
+responseRouter.get("/survey/:id", extractJWT, async (req: Request, res: Response, next: NextFunction) => {
+    const id = parseInt(req.params.id);
+    if (id) {
+        const responses = await dbMethods.responseMethods.getResponsesBySurveyId(id)
+        if(responses) {
+            res.json(responses)
+        } else {
+            res.sendStatus(400);
+        }
+    } else {
+        res.json("no survey id... that's odd");
+    }
+    // console.log(req.body + 'survey by survey id route accessed');
+})
+
 responseRouter.post("/", async (req: Request, res: Response, next: NextFunction) => {
     console.log('hereeeeee')
     const {id, responses} = req.body;
-    const myResponses = responses.map((response: any) => {
-        response.answer
-    })
-    if (id && myResponses.length >= 1) {    
-        const postResponse = await dbMethods.responseMethods.addResponse(id, myResponses);
+    console.log(responses);
+    // const myResponses = responses.map((response: any) => {
+    //     response.answer
+    // })
+    if (id && responses.length >= 1) {    
+        const postResponse = await dbMethods.responseMethods.addResponse(id, responses);
         if (postResponse) {
             console.log(JSON.stringify(postResponse) + " WAS POSTED TO DB")
             res.sendStatus(200)
